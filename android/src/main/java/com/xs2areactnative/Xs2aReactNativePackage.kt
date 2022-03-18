c75bfbd4b285d4220952ee6204e4c0b4a07ce06e
Maik Mursall <mmursall@fintecsystems.com>
2022-03-18 15:38:07 +0100

    Added Android Native Module

--- android/src/main/java/com/xs2areactnative/Xs2aReactNativePackage.kt
+++ android/src/main/java/com/xs2areactnative/Xs2aReactNativePackage.kt
@@ -8,7 +8,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class Xs2aReactNativePackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return emptyList()
+        return listOf(Xs2aReactNativeModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
